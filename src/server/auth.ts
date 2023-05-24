88b12d809fd82d97fea2a34cc5bfb2c4c7f28614
JaacielBriseo <jaacielbris23@gmail.com>
2023-05-23 20:55:22 -0700

    Allow multiple emails

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -50,6 +50,7 @@ export const authOptions: NextAuthOptions = {
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     /**
      * ...add more providers here.
