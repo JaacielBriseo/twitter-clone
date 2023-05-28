de8e8d77c256799b9a5ba831a61bc14417ffcb08
JaacielBriseo <jaacielbris23@gmail.com>
2023-05-27 17:40:49 -0700

    Fix error on getSSG function

--- src/server/api/getSSG.ts
+++ src/server/api/getSSG.ts
@@ -2,7 +2,7 @@ import { createServerSideHelpers } from "@trpc/react-query/server";
 import { appRouter } from "./root";
 import superjson from "superjson";
 import { createInnerTRPCContext } from "./trpc";
-export const ssgHelper = () =>  {
+export const getSSG = () =>  {
   return createServerSideHelpers({
     router: appRouter,
     ctx: createInnerTRPCContext({ session: null, revalidateSSG: null }),
